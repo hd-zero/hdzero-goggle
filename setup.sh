a75fb24e758e572cdf27992dbe633ac150d3ff82
bkleiner <b.kleiner@str8labs.com>
2022-12-12 13:23:39 +0100

    fetch up-to-date toolchain from musl.cc, automate setup completely

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
-if [ ! -d build ]
-then
-	mkdir build
-else
-	rm build/* -rf
+if [[ ! -d linux-4.9 ]]; then
+	echo "installing linux headers..."
+	tar xzf sdk/linux-4.9.tar.gz
 fi
-cd build && cmake .. --toolchain=../toolchain.cmake
+
+if [[ ! -d media ]]; then
+	echo "installing media libraries..."
+	cat sdk/media.tar.gzaa sdk/media.tar.gzab | tar xz 
+fi
+
+if [[ ! -d toolchain ]]; then
+	echo "installing toolchain..."
+	mkdir toolchain
+	wget -qO- http://musl.cc/arm-linux-musleabihf-cross.tgz | tar xz -C toolchain
+fi
+
+rm -rf build && mkdir build
+cmake . --toolchain=toolchain.cmake -Bbuild
--- toolchain.cmake
+++ toolchain.cmake
@@ -1,10 +1,17 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
- 
-set(TOOLS ${PROJECT_SOURCE_DIR}/toolchain)
-set(CMAKE_C_COMPILER "${TOOLS}/bin/arm-openwrt-linux-muslgnueabi-gcc")
-set(CMAKE_CXX_COMPILER "${TOOLS}/bin/arm-openwrt-linux-muslgnueabi-g++")
-set(CMAKE_AR "${TOOLS}/bin/arm-openwrt-linux-muslgnueabi-ar")
-set(CMAKE_LD "${TOOLS}/bin/arm-openwrt-linux-muslgnueabi-ld")
- 
-add_compile_options(-Wall)
+
+set(TOOLS ${PROJECT_SOURCE_DIR}/toolchain/arm-linux-musleabihf-cross)
+
+set(CMAKE_FIND_ROOT_PATH ${TOOLS})
+set(CMAKE_SYSROOT ${TOOLS})
+
+set(CMAKE_AR                        ${TOOLS}/bin/arm-linux-musleabihf-ar)
+set(CMAKE_ASM_COMPILER              ${TOOLS}/bin/arm-linux-musleabihf-gcc)
+set(CMAKE_C_COMPILER                ${TOOLS}/bin/arm-linux-musleabihf-gcc)
+set(CMAKE_CXX_COMPILER              ${TOOLS}/bin/arm-linux-musleabihf-g++)
+set(CMAKE_LINKER                    ${TOOLS}/bin/arm-linux-musleabihf-ld)
+set(CMAKE_OBJCOPY                   ${TOOLS}/bin/arm-linux-musleabihf-objcopy CACHE INTERNAL "")
+set(CMAKE_RANLIB                    ${TOOLS}/bin/arm-linux-musleabihf-ranlib CACHE INTERNAL "")
+set(CMAKE_SIZE                      ${TOOLS}/bin/arm-linux-musleabihf-size CACHE INTERNAL "")
+set(CMAKE_STRIP                     ${TOOLS}/bin/arm-linux-musleabihf-strip CACHE INTERNAL "")
