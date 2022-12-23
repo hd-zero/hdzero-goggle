155b3b8b81a4d700fc24c45e38a1d875a21043a7
bkleiner <b.kleiner@str8labs.com>
2022-12-23 15:57:21 +0000

    isolate non-standard linux headers into /lib this now allows for compiling the meda files into the application

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ ! -d linux-4.9 ]; then
-	echo "Extracting linux headers..."
-	tar xzf linux-4.9.tar.gz
-fi
-
 if [ ! -d media ]; then
 	echo "Extracting media libraries..."
 	cat media.tar.gzaa media.tar.gzab | tar xz 
