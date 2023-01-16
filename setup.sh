e51aa024f725f5981a00618b76bf328e8e86da1d
bkleiner <b.kleiner@str8labs.com>
2023-01-16 20:57:36 +0100

    switch to buildroot toolchain

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 
+TOOLCHAIN_URL="https://toolchains.bootlin.com/downloads/releases/toolchains/armv7-eabihf/tarballs/armv7-eabihf--musl--stable-2018.02-2.tar.bz2"
+
 if [ ! -d toolchain ]; then
 	echo "Extracting toolchain..."
-	tar xzvf toolchain.tar.gz
+	mkdir toolchain
+	wget -qO- "$TOOLCHAIN_URL" | tar xj --strip-components=1 -C toolchain
 fi
 
 rm -rf build && mkdir build
-cmake . -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake -Bbuild
+cmake . -DCMAKE_TOOLCHAIN_FILE=toolchain/share/buildroot/toolchainfile.cmake -Bbuild
