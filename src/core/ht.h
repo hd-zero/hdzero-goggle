040a86e8248d48b747cd8975b1db8e3552ea02c3
Paul Kendall <pkendall64@gmail.com>
2023-01-18 09:36:47 +1300

    Code cleanup

--- src/core/ht.h
+++ src/core/ht.h
@@ -39,16 +39,6 @@ typedef struct {
     float   panFactor;
 
     //PPM setting
-    int16_t tiltMaxPulse;
-    int16_t tiltMinPulse;
-    int16_t tiltCenter;
-    int16_t panMaxPulse;
-    int16_t panMinPulse;
-    int16_t panCenter;
-    int16_t rollMaxPulse;
-    int16_t rollMinPulse;
-    int16_t rollCenter;
-
     int16_t htChannels[3]; //0=Pan, 1=tilt, 2=roll
     
     // internal state
