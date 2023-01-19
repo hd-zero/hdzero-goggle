2f22e1da8e93745ca6b465aa9b09684f0bca391f
bkleiner <b.kleiner@str8labs.com>
2023-01-19 14:18:49 +0100

    formatsd: save and restore resources, fixes #75

--- mkapp/app/script/formatsd.sh
+++ mkapp/app/script/formatsd.sh
@@ -3,11 +3,22 @@
 source /mnt/app/app/record/record-env.sh
 /mnt/app/app/record/gogglecmd -rec quit
 /mnt/app/app/record/gogglecmd -sds quit
+
+if [ -e /mnt/extsd/resource ]; then
+    cp -r /mnt/extsd/resource /tmp/
+fi
+
 sleep 2
 umount /mnt/extsd
 mkfs.vfat -F 32 /dev/mmcblk0
 mount /dev/mmcblk0 /mnt/extsd
 sleep 1
+
+if [ -e /tmp/resource ]; then
+    cp -r /tmp/resource /mnt/extsd/
+    rm -rf /tmp/resource
+fi
+
 /mnt/app/app/record/record &
 /mnt/app/app/record/sdstat &
 
