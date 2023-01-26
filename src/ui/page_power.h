9c533a99d8e8ecffd0f26966104b984e091385cf
Nikolas Saf <nikolas.saf@gmail.com>
2023-01-26 15:08:40 +0100

    improve variable naming, inc max cell count to 6 also removed g_battery.type = 2; from device_init() in main.c, to prevent auto detected cell count from being overwritten

--- src/ui/page_power.h
+++ src/ui/page_power.h
@@ -1,14 +1,14 @@
 #ifndef _PAGE_POWER_H
 #define _PAGE_POWER_H
 
-#define S_MIN_COUNT 2
-#define S_MAX_COUNT 5
+#define CELL_MIN_COUNT 2
+#define CELL_MAX_COUNT 6
 #define CELL_VOLTAGE_MIN 28
 #define CELL_VOLTAGE_MAX 42
 
 #define ROW_BATT_C_LABEL 0
-#define ROW_S_COUNT_MODE 1
-#define ROW_S_COUNT 2
+#define ROW_CELL_COUNT_MODE 1
+#define ROW_CELL_COUNT 2
 #define ROW_CELL_VOLTAGE 3
 #define ROW_WARN_TYPE 4
 #define ROW_BACK 5
@@ -21,10 +21,10 @@
 
 extern page_pack_t pp_power;
 
-void power_s_count_inc(void);
-void power_s_count_dec(void);
+void power_cell_count_inc(void);
+void power_cell_count_dec(void);
 void power_voltage_inc(void);
 void power_voltage_dec(void);
-void set_battery_S();
+void set_battery_cell_count();
 
 #endif
