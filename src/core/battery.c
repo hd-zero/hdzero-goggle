653e01df664dec59ab82cfd68d88b67babed7772
bkleiner <b.kleiner@str8labs.com>
2023-01-28 01:19:01 +0100

    refactor battery logic into dedicated file

---
+++ src/core/battery.c
@@ -0,0 +1,32 @@
+#include "battery.h"
+
+#include "driver/mcp3021.h"
+#include "ui/page_common.h"
+
+sys_battery_t g_battery;
+
+static int battery_detect_type() {
+    int v = mcp_read_voltage();
+    return (v * 10 / 1000 / 42 + 1);
+}
+
+void battery_init() {
+    switch (g_setting.power.cell_count_mode) {
+    default:
+    case SETTING_POWER_CELL_COUNT_MODE_AUTO:
+        g_battery.type = battery_detect_type();
+        break;
+    case SETTING_POWER_CELL_COUNT_MODE_MANUAL:
+        g_battery.type = g_setting.power.cell_count;
+        break;
+    }
+}
+
+void battery_update() {
+    g_battery.voltage = mcp_read_voltage();
+}
+
+bool battery_is_low() {
+    int cell_volt = g_battery.voltage / g_battery.type;
+    return cell_volt <= g_setting.power.voltage * 100;
+}
