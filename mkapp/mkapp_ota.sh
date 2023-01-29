fe1a47b1f1384676bec5b27417829708f0d774fa
bkleiner <b.kleiner@str8labs.com>
2023-01-29 02:06:17 +0100

    fix app size, for some reason the short versions do not work reliable

--- mkapp/mkapp_ota.sh
+++ mkapp/mkapp_ota.sh
@@ -20,18 +20,19 @@ BIN_DIR=$ROOT_DIR/bin
 IMG_DIR=$ROOT_DIR/ota_app
 APP_DIR=$ROOT_DIR/app
 
-APP_SIZE=12582912
+APP_SIZE=8388608
 APP_IMAGE=${IMG_DIR}/app.fex
 APP_VERSION=$(get_app_version "$ROOT_DIR/app/version")
 
 rm -rf $IMG_DIR
 mkdir -p $IMG_DIR
 
-${BIN_DIR}/mkfs.jffs2 -l -e 0x10000 \
-	-p ${APP_SIZE} \
-	-d ${APP_DIR} \
-	-o ${APP_IMAGE}
-
+${BIN_DIR}/mkfs.jffs2 \
+    --little-endian \
+    --eraseblock=0x10000 \
+    --pad=${APP_SIZE} \
+    --root=${APP_DIR} \
+    --output=${APP_IMAGE}
 make_img_md5 ${APP_IMAGE}
 
 cd $IMG_DIR
