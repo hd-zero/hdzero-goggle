d6fca54d13a4b85bdedcb900e9e75dcda3bfb20c
bkleiner <b.kleiner@str8labs.com>
2023-02-28 14:53:43 +0100

    format thread.{c,h}

--- src/core/thread.h
+++ src/core/thread.h
@@ -1,17 +1,17 @@
 #ifndef _THREAD_H
 #define _THREAD_H
 
-#include <stdint.h>
 #include <pthread.h>
+#include <stdint.h>
 
 #define THREAD_COUNT_MAX (10)
-#define THREAD_COUNT (3)
+#define THREAD_COUNT     (3)
 
 typedef void *(*fun_thread_instance_t)(void *params);
 
 typedef struct {
-	pthread_t pid[THREAD_COUNT_MAX]; 
-	fun_thread_instance_t instance[THREAD_COUNT_MAX];
+    pthread_t pid[THREAD_COUNT_MAX];
+    fun_thread_instance_t instance[THREAD_COUNT_MAX];
 } threads_obj_t;
 
 int create_threads();
