dcfa0d0c5d3fadaf564fad25687d4f63c6e4f48f
Paulo Cabral <sumolx@gmail.com>
2023-03-10 07:59:38 -0500

    Comments adjudicated. Added ability to force RTC update when year is 1970.

--- src/driver/rtc.h
+++ src/driver/rtc.h
@@ -2,17 +2,17 @@
 #define _RTC_H
 
 struct rtc_date {
-	int year;  // YYYY
-	int month; // 1 - 12
-	int day;   // 1 - 31
-	int hour;  // 0 - 23
-	int min;   // 0 - 59 
-	int sec;   // 0 - 59
+    int year;  // YYYY
+    int month; // 1 - 12
+    int day;   // 1 - 31
+    int hour;  // 0 - 23
+    int min;   // 0 - 59
+    int sec;   // 0 - 59
 };
 
 void rct_init();
 void rtc_timestamp();
-void rtc_set_clock(const struct rtc_date* rd);
-void rtc_get_clock(struct rtc_date* rd);
+void rtc_set_clock(const struct rtc_date *rd);
+void rtc_get_clock(struct rtc_date *rd);
 
 #endif
