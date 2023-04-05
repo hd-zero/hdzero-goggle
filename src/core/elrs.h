10872139cfaf6d0be9f6b63f76a10d099443c700
FOG_Yamato <contact@cybersnets.com>
2023-04-06 00:00:10 +0300

    Add clock update function via ELRS backpack

--- src/core/elrs.h
+++ src/core/elrs.h
@@ -43,6 +43,7 @@ typedef struct __attribute__((packed)) {
 #define MSP_GET_VERSION    0x030A
 #define MSP_SET_BUZZER     0x030B
 #define MSP_SET_HT_ENABLE  0x030D
+#define MSP_SET_RTC        0x030E
 #define MSP_SET_OSD_ELEM   0x00B6
 #define MSP_SET_MODE       0x0380 // goggles to backpack
 #define MSP_GET_BP_VERSION 0x0381 // goggles to backpack
