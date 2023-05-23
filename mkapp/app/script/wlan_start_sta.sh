9ff7ff1de6b820427328d80e434e63aa556eef49
Paulo Cabral <sumolx@gmail.com>
2023-05-23 09:29:22 -0400

    Removed wlan start script, generated via wifi page

--- mkapp/app/script/wlan_start_sta.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-insmod /mnt/app/ko/xradio_mac.ko
-insmod /mnt/app/ko/xradio_core.ko
-insmod /mnt/app/ko/xradio_wlan.ko
-
-ifconfig wlan0 up
-mkdir /var/log
-wpa_supplicant -Dnl80211 -iwlan0 -c/tmp/wpa_supplicant.conf&
