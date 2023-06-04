8e53deb29496704af6d1898c7d8cf143544ec7c8
Paulo Cabral <sumolx@gmail.com>
2023-06-04 17:21:23 -0400

    Now utilizing udev rules for fsck

--- mkapp/app/services/scripts/dosfstools_startup.sh
+++ mkapp/app/services/scripts/dosfstools_startup.sh
@@ -4,6 +4,13 @@ ln -sfn /mnt/app/services/dosfstools/fatlabel /bin/fatlabel
 ln -sfn /mnt/app/services/dosfstools/fsck.fat /bin/fsck.fat
 ln -sfn /mnt/app/services/dosfstools/mkfs.fat /bin/mkfs.fat
 
-# Repair SD Card if needed
-/bin/fsck.fat -a /dev/mmcblk0 > /tmp/fsck.log 2>&1
-mv /tmp/fsck.log /mnt/extsd/
+# Generate new rule if it does not exist.
+if [ ! -e /etc/udev/rules.d/11-extsd-v1.rules ]; then
+    rm -rf /etc/udev/rules.d/11-extsd-*
+
+    echo "ACTION==\"add\", KERNEL==\"mmcblk[0-9]\", SUBSYSTEM==\"block\", RUN+=\"/mnt/app/script/repairsd.sh\"" > /etc/udev/rules.d/11-extsd-v1.rules
+    echo "ACTION==\"add\", KERNEL==\"mmcblk[0-9]p[0-9]\", SUBSYSTEM==\"block\", RUN+=\"/mnt/app/script/repairsd.sh\"" >> /etc/udev/rules.d/11-extsd-v1.rules
+
+    udevadm control --reload-rules
+fi
+
