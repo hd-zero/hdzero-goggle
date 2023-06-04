9b324ca55e333fee1f71eb927c0e64ee84a84f36
Brandon Geraci <brandon.geraci@gmail.com>
2023-06-04 15:27:17 -0500

    Modify script to handle different errors

--- mkapp/app/script/repairsd.sh
+++ mkapp/app/script/repairsd.sh
@@ -1,5 +1,41 @@
 #!/bin/sh
 
-rm -rf /tmp/repairsd.done
-/bin/fsck.fat -a /dev/mmcblk0 > /tmp/fsck.log 2>&1
-touch /tmp/repairsd.done
+# Define variables for log and done file paths
+LOGFILE="/tmp/fsck.log"
+DONEFILE="/tmp/repairsd.done"
+
+# Define the device to check
+DEVICE="/dev/mmcblk0"
+
+# Start by removing the done file if it exists
+rm -rf ${DONEFILE}
+
+# Run fsck on the device, capturing output and exit status
+/bin/fsck.fat -a ${DEVICE} > ${LOGFILE} 2>&1
+EXITSTATUS=$?
+
+# Add fsck exit status to the log file
+echo "fsck exit status: ${EXITSTATUS}" >> ${LOGFILE}
+
+# Depending on the exit status, handle different cases
+case ${EXITSTATUS} in
+    0)
+        # No errors were detected, or the errors were corrected without intervention
+        touch ${DONEFILE}
+        ;;
+    1)
+        # Errors were found and corrected, but user intervention may be needed
+        # You can decide how to handle this case
+        ;;
+    2)
+        # Errors were found but not corrected. The filesystem may be corrupt.
+        # You can decide how to handle this case
+        ;;
+    *)
+        # Some other exit status was returned. This could indicate a different kind of error.
+        # You can decide how to handle this case
+        ;;
+esac
+
+# The script will end here. If necessary, you can add commands to perform additional actions,
+# like notifying the user of the error, or attempting to mount the filesystem read-only.
