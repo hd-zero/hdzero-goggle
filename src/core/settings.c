772f77e0af89ff7503c1233dc517b36e310b49fd
Tomohisa Kusukawa <t@kusukawa.jp>
2023-08-01 23:17:42 +0900

    change font of OSD:Date,Time

--- src/core/settings.c
+++ src/core/settings.c
@@ -113,7 +113,7 @@ const setting_t g_setting_defaults = {
             // OSD_GOGGLE_CLOCK_TIME
             {
                 .show = true,
-                .position = {.mode_4_3 = {.x = 570, .y = 24}, .mode_16_9 = {.x = 410, .y = 24}},
+                .position = {.mode_4_3 = {.x = 540, .y = 24}, .mode_16_9 = {.x = 380, .y = 24}},
             },
             // OSD_GOGGLE_CHANNEL
             {
