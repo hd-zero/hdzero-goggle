7c6e98d45edf8770b8f5d6abf472ab519ff04768
Paulo Cabral <sumolx@gmail.com>
2023-08-15 09:12:45 -0400

    Online Updater Backend

---
+++ mkapp/app/script/online_downloader.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+
+version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
+version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" == "$1"; }
+version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
+version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
+
+download() {
+    dst_path="$1"
+    fw_links="$2"
+    fw_sizes="$3"
+    fw_notes="$4"
+    fw_total=1
+    fw_counter=1
+
+    # Do we have a SD Card mounted?
+    if [ ! -z "$(mount | grep /mnt/extsd)" ]; then
+        dst_path="/mnt/extsd/FIRMWARE/$dst_path"
+    else
+        dst_path="/tmp/FIRMWARE/$dst_path"
+    fi
+
+    # Check if we need to download?
+    if test -f $dst_path/release.notes; then
+        echo "SD Card contains latest release!"
+        return 0
+    fi
+
+    # Blindly create directory, errors checking handled below.
+    mkdir -p $dst_path 2> /dev/null
+
+    # The real download work is performed here!
+    for link in $fw_links; do
+        file="$(basename "$link")"
+        target="$dst_path/$file"
+        if test -f "$target"; then
+            current_size=$(wc -c < "$target")
+            archive_size=$(echo $fw_sizes | cut -d" " -f$fw_counter)
+            if [ $current_size -ne $archive_size ]; then
+                echo "Resuming download: $target ...."
+                curl -s -k -C $current_size -L $link -o "$target" && \
+                fw_total=$((fw_total + 1)) && \
+                echo "Resuming download: $target DONE!"
+            else
+                fw_total=$((fw_total + 1))
+            fi
+        else
+            echo "Downloading firmware: $target ...."
+            curl -s -k -L $link -o "$target" && \
+            fw_total=$((fw_total + 1)) && \
+            echo "Downloading firmware: $target DONE!"
+        fi
+        fw_counter=$((fw_counter + 1))
+    done
+
+    # Having release notes identify a successful download.
+    if [ $fw_total -eq $fw_counter ]; then
+        echo "$fw_notes" > $dst_path/release.notes && \
+        echo "Completed downloading latest release!"
+    fi
+}
+
+app_version_check() {
+    if version_gt $1 $2; then
+        echo "Goggle running older firmware!"
+        return 0
+    else
+        echo "Goggle running latest firmware!"
+        return 1
+    fi
+}
+
+progress_indicator() {
+    if [ "$1" = "set" ]; then
+        touch /tmp/online_downloader.inprogress
+    elif [ "$1" = "clr" ]; then
+        rm -rf /tmp/online_downloader.inprogress
+    fi
+}
+
+online_goggle_fw_check() {
+    fw_info="/tmp/hdz_goggle_fw.latest"
+    echo "Checking Goggle Releases" && \
+    curl -ks -o $fw_info https://api.github.com/repos/hd-zero/hdzero-goggle/releases/latest && \
+    fw_link=$(cat $fw_info | grep 'browser_' | cut -d\" -f4) && \
+    fw_note=$(cat $fw_info | grep 'body' | cut -d\" -f4) && \
+    fw_size=$(cat $fw_info | grep 'size' | cut -d\" -f4) && \
+    fw_file=$(basename $fw_link) && \
+    rversion="$(echo ${fw_file%%.bin} | cut -d "-" -f4)" && \
+    lversion="$(cat /mnt/app/version | cut -d "-" -f1)" && \
+    app_version_check $rversion $lversion && \
+    echo "Searching SD Card for latest release..." && \
+    progress_indicator "set" && \
+    download "GOGGLE/$rversion" "$fw_link" "$fw_size" "$fw_note"
+    progress_indicator "clr"
+}
+
+online_vtx_fw_check() {
+    fw_info="/tmp/hdz_vtx_fw.latest"
+    echo "Checking VTX Releases" && \
+    curl -ks -o $fw_info https://api.github.com/repos/hd-zero/hdzero-vtx/releases/latest && \
+    fw_links=$(cat $fw_info | grep 'browser_' | cut -d\" -f4) && \
+    fw_notes=$(cat $fw_info | grep 'body' | cut -d\" -f4) && \
+    fw_sizes=$(cat $fw_info | grep 'size' | cut -d: -f2  | cut -d, -f1) && \
+    rversion=$(cat $fw_info | grep 'tag_name' | cut -d\" -f4) && \
+    echo "Searching SD Card for latest release..." && \
+    progress_indicator "set" && \
+    download "VTX/$rversion" "$fw_links" "$fw_sizes" "$fw_notes"
+    progress_indicator "clr"
+}
+
+online_goggle_fw_check
+online_vtx_fw_check
---
+++ mkapp/app/services/scripts/04_tinycurl_startup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ln -sfn /mnt/app/services/tinycurl/bin/curl /bin/curl
