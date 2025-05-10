b42b1a414274a9a4b8f5ad0718c56517689e1cf4
pitts-mo <pitts.mo@gmail.com>
2025-05-10 11:17:19 -0400

    Update update_goggle.sh -disable mtd write success beeps -simplified
    -disable mtd write success beeps
    -simplified
    -notable change: now erase entire mtd before write (prior code only erased initial 65536 of rx mtd)

--- mkapp/app/script/update_goggle.sh
+++ mkapp/app/script/update_goggle.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-GOGGLE_BIN="$1"
-TMP_DIR=/tmp/goggle_update
+VAbin=/mnt/extsd/HDZGOGGLE_VA.bin
+VAcount=1
+VAwrites=0
+RXbin=/mnt/extsd/HDZGOGGLE_RX.bin
+RXcount=2
+RXwrites=0
 
 function gpio_export()
 {
@@ -27,33 +31,46 @@ function gpio_export()
         echo "out">/sys/class/gpio/gpio131/direction
 }
 
+function beep()
+{
+	if [ ! -z "$1" ];then
+	delay=$1
+	else
+	delay=1
+	fi
+	echo "1">/sys/class/gpio/gpio131/value
+	sleep $delay
+        echo "0">/sys/class/gpio/gpio131/value
+}
+
 function beep_success()
 {
-    	echo "1">/sys/class/gpio/gpio131/value
-		sleep 0.1
-    	echo "0">/sys/class/gpio/gpio131/value
-		sleep 0.5
-    	echo "1">/sys/class/gpio/gpio131/value
-		sleep 0.05
-    	echo "0">/sys/class/gpio/gpio131/value
+	beep 0.1
+	sleep 0.5
+	beep 0.05
+	sleep 1
 }
 
 
 function beep_failure()
 {
-    	echo "1">/sys/class/gpio/gpio131/value
-		sleep 1
-    	echo "0">/sys/class/gpio/gpio131/value
-		sleep 0.5
-    	echo "1">/sys/class/gpio/gpio131/value
-		sleep 1
-    	echo "0">/sys/class/gpio/gpio131/value
-		sleep 0.5
-    	echo "1">/sys/class/gpio/gpio131/value
-		sleep 0.05
-    	echo "0">/sys/class/gpio/gpio131/value
+	beep 1
+	sleep 0.5
+	beep 1
+	sleep 0.5
+	beep 0.05
+	sleep 1
 }
 
+function disconnect_fpga_flash()
+{
+        echo "1">/sys/class/gpio/gpio258/value
+}
+
+function connect_fpga_flash()
+{
+        echo "0">/sys/class/gpio/gpio258/value
+}
 
 function gpio_set_reset()
 {
@@ -67,122 +84,97 @@ function gpio_clear_reset()
         echo "0">/sys/class/gpio/gpio228/value
 }
 
-function gpio_set_send()
-{
-        echo "1">/sys/class/gpio/gpio224/value
-        echo "0">/sys/class/gpio/gpio228/value
-}
-
-function disconnect_fpga_flash()
-{
-        echo "1">/sys/class/gpio/gpio258/value
-}
-
-function connect_fpga_flash()
-{
-        echo "0">/sys/class/gpio/gpio258/value
-}
-
-function untar_file()
-{
-	FILE_TARGET="$1"
-
-	if [ ! -e ${TMP_DIR} ]
-	then
-		mkdir ${TMP_DIR}
-	else
-		rm ${TMP_DIR} -rf
-		mkdir ${TMP_DIR}
-	fi
-
-	tar xf ${FILE_TARGET} -C ${TMP_DIR} 2>&1 > /dev/null
-	mv ${TMP_DIR}/HDZGOGGLE_RX*.bin ${TMP_DIR}/HDZGOGGLE_RX.bin
-	mv ${TMP_DIR}/HDZGOGGLE_VA*.bin ${TMP_DIR}/HDZGOGGLE_VA.bin
-}
- 
-# eg: check_mtd_write /dev/mtdX check-size erase-size file-size bin-file
+# eg: check_mtd_write /dev/mtdX required-size bin-file
 function check_mtd_write()
 {
+	filesize=`ls -l $3 | awk '{print $5}'`
 	mtd_info=`mtd_debug info $1`
 	echo "$mtd_info"
-	value=`echo "$mtd_info" | grep mtd.size | grep "($2)"`                
-	if [ ! -z "$value" ];then
-	    echo "$1 size is ($2)" 
-		mtd_debug erase $1 0 $3
-		mtd_debug write $1 0 $4 $5
-		beep_success
+	mtdsize=`echo "$mtd_info" | grep mtd.size | grep "($2)"`                
+	if [ ! -z "$mtdsize" ];then
+	        echo "$1 size is ($2)" 
+		mtdsizeB=`echo "$mtdsize" |cut -d " " -f 3`
+		echo mtd_debug erase $1 0 $mtdsizeB
+		mtd_debug erase $1 0 $mtdsizeB
+		echo mtd_debug write $1 0 $filesize $3
+		mtd_debug write $1 0 $filesize $3
+		if [ $? == 0 ]; then
+			beep_success
+			if [ "$3" == "$VAbin" ]; then
+				VAwrites=$((VAwrites + 1))
+			fi
+			if [ "$3" == "$RXbin" ]; then
+				RXwrites=$((RXwrites + 1))
+			fi
+		fi 
 	else
-	    echo "$1 size is NOT ($2) !" 
+	        echo "$1 size is NOT ($2) !" 
 		beep_failure
 	fi
 }
 
-function update_rx()
-{
-	echo "find RX update file, start update"
-	filesize=`ls -l ${TMP_DIR}/HDZGOGGLE_RX*.bin| awk '{print $5}'`
-	gpio_export
-	gpio_set_reset
-	insmod /mnt/app/ko/w25q128.ko
-	check_mtd_write /dev/mtd8 1M 65536 $filesize ${TMP_DIR}/HDZGOGGLE_RX.bin
-    echo "5"                                                                          
-    echo "5" > /tmp/progress_goggle
-	sleep 1
-	check_mtd_write /dev/mtd9 1M 65536 $filesize ${TMP_DIR}/HDZGOGGLE_RX.bin
-    echo "10"                                                                          
-    echo "10" > /tmp/progress_goggle
-	echo "update finish RX, running"
-	gpio_clear_reset
-	sleep 1
-	rmmod /mnt/app/ko/w25q128.ko
-}
 
-function update_fpga()
-{
-	echo "find VA update file, start update"
-	filesize2=`ls -l ${TMP_DIR}/HDZGOGGLE_VA*.bin| awk '{print $5}'`
-	gpio_export
-	gpio_set_reset
-	disconnect_fpga_flash
-	insmod /mnt/app/ko/w25q128.ko
-	check_mtd_write /dev/mtd10 16M 16777216 $filesize2 ${TMP_DIR}/HDZGOGGLE_VA.bin
-    echo "45"                                                                         
-    echo "45" > /tmp/progress_goggle  
-	echo "update finish VA, running"
-	gpio_clear_reset
-	sleep 1
-	rmmod /mnt/app/ko/w25q128.ko
-}
+echo "<<<<-------------------------------------------------------------------->>>>"
 
-# If firmware file was NOT supplied then default to primary location for emergency restore
-if [ -z "$GOGGLE_BIN" ]; then
-    if [ `ls /mnt/extsd/HDZERO_GOGGLE*.bin | grep bin | wc -l` -eq 1 ]
-    then
-        GOGGLE_BIN="/mnt/extsd/HDZERO_GOGGLE*.bin"
-    fi
-fi
-
-if [ ! -z "$GOGGLE_BIN" ]
+if [ -e $VAbin ]
 then
-	echo "Flashing $GOGGLE_BIN"
-	echo "0" > /tmp/progress_goggle
-	echo "0"
-	untar_file "$GOGGLE_BIN"
-	mv ${TMP_DIR}/hdzgoggle_app_ota*.tar ${TMP_DIR}/hdzgoggle_app_ota.tar
-	cp -f /mnt/app/setting.ini /mnt/UDISK/
-	#disable it66021
-	i2cset -y 3 0x49 0x10 0xff
-        update_rx
-	update_fpga
-	hdz_upgrade_app.sh
-	echo "100"
-	echo "100" > /tmp/progress_goggle
-	echo "all done"
+    echo "find VA update file, start update"
+    gpio_export
+    gpio_set_reset
+    disconnect_fpga_flash
+		sleep 1
+    insmod /mnt/app/ko/w25q128.ko
+
+		touch /tmp/update.ing
+    # normally the VA is mounted at /dev/mtd10 
+    #	check_mtd_write /dev/mtd8 16M $VAbin
+    #	check_mtd_write /dev/mtd9 16M $VAbin
+	  check_mtd_write /dev/mtd10 16M $VAbin
+		rm /tmp/update.ing -rf
+
+		if [ "$VAwrites" == "$VAcount" ]; then
+			if [ ! -f /mnt/extsd/DONOTREMOVE.txt ]
+			then
+				rm $VAbin -rf
+			fi
+		fi
+
+    echo "update finish $VAwrites VA, running"
+    gpio_clear_reset
+	  sleep 1
+    rmmod /mnt/app/ko/w25q128.ko
 else
-	if [ `ls /mnt/extsd/HDZERO_GOGGLE*.bin | grep bin | wc -l` -eq 0 ]
-	then
-		echo "skip"
-	else
-		echo "repeat"
-	fi
+    echo "no VA update file,skip" 
 fi
+
+
+if [ -e $RXbin ]
+then
+    echo "find RX update file, start update"
+    gpio_export
+    gpio_set_reset
+		sleep 1
+    insmod /mnt/app/ko/w25q128.ko
+    # normally rx1 is /dev/mtd8 and rx0 is /dev/mtd9
+	  check_mtd_write /dev/mtd8 1M $RXbin
+	  check_mtd_write /dev/mtd9 1M $RXbin
+    #	check_mtd_write /dev/mtd10 1M $RXbin
+
+		if [ "$RXwrites" == "$RXcount" ]; then
+			if [ ! -f /mnt/extsd/DONOTREMOVE.txt ]
+			then
+				rm $RXbin -rf
+			fi
+		fi
+
+    echo "update finish $RXwrites RX, running"
+    gpio_clear_reset
+		sleep 1
+    rmmod /mnt/app/ko/w25q128.ko
+		
+		beep 2
+else
+    echo "no RX update file,skip"
+fi
+
+echo "<<<<-------------------------------------------------------------------->>>>"
