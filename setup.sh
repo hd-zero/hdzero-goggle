d2d0f735f5f93665e1b272903739a31fba860801
pitts-mo <pitts.mo@gmail.com>
2025-06-22 15:21:37 -0400

    Update setup.sh -add toolchain awareness
    updates to setup.sh script will now:
    -prompt the source code user before downloading toolchain (50mb)
    -add verbosity for download and extraction status
    -preserve downloaded toolchain .tar.bz2 file for future use
    -allow user to pass full path of toolchain file or manually copy toolchain file into root of source to prevent prompting or new download of toolchain

--- setup.sh
+++ setup.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
-set -e
+echo "Working directory: `pwd`"
 
-TOOLCHAIN_URL="https://toolchains.bootlin.com/downloads/releases/toolchains/armv7-eabihf/tarballs/armv7-eabihf--musl--stable-2018.02-2.tar.bz2"
+if [ ! -z "$1" ]; then
+cp $1 .
+fi
 
-if [ ! -d toolchain ]; then
+TOOLCHAIN_FILE="armv7-eabihf--musl--stable-2018.02-2.tar.bz2"
+TOOLCHAIN_URL="https://toolchains.bootlin.com/downloads/releases/toolchains/armv7-eabihf/tarballs/$TOOLCHAIN_FILE"
+
+if [ ! -f "$TOOLCHAIN_FILE" ]; then
+  echo "$TOOLCHAIN_FILE not found."
+  while true; do
+    read -p "download $TOOLCHAIN_URL? (y/n): " yn
+      case $yn in
+        [yY]* ) wget "$TOOLCHAIN_URL"; break;; # Accepts 'y', 'Y', 'yes', 'YES', etc.
+        [nN]* ) echo "Please run setup.sh with path to toolchain archive eg: "; echo "./setup.sh $TOOLCHAIN_FILE"; exit;; # Accepts 'n', 'N', 'no', 'NO', etc.
+        * ) echo "Invalid response. Please enter 'y' or 'n'.";;
+      esac
+  done
+fi
+if [ -f "$TOOLCHAIN_FILE" ]; then  
+  echo "Using toolchain `pwd`/$TOOLCHAIN_FILE"
+  if [ ! -d toolchain ]; then
 	echo "Extracting toolchain..."
 	mkdir toolchain
-	wget -qO- "$TOOLCHAIN_URL" | tar xj --strip-components=1 -C toolchain
+	tar -xvjf $TOOLCHAIN_FILE --strip-components=1 -C toolchain
+  fi
 fi
 
 rm -rf build_*
