9b930b369856116828f549b7e84ad9ffbcdf2021
Master92 <nils.friedchen@googlemail.com>
2025-11-03 12:48:03 +0100

    Split Spanish characters and annotate

--- utilities/font/analyze_and_add_spanish_chars.py
+++ utilities/font/analyze_and_add_spanish_chars.py
@@ -11,7 +11,23 @@ target_font_file = 'Montserrat-Medium.ttf'  # Fuente TTF
 output_dir = 'out'  # Directorio de salida
 
 # Caracteres españoles a agregar (puntos Unicode)
-spanish_unicode_points = [161, 191, 193, 201, 205, 209, 211, 218, 220, 225, 233, 237, 241, 243, 250, 252]
+spanish_unicode_points = [161, # ¡
+                          191, # ¿
+                          193, # Á
+                          201, # È
+                          205, # Í
+                          209, # Ñ
+                          211, # Ó
+                          218, # Ú
+                          220, # Ü
+                          225, # á
+                          233, # é
+                          237, # í
+                          241, # ñ
+                          243, # ó
+                          250, # ú
+                          252  # ü
+                          ]
 
 def extract_simplified_chinese_unicode():
     """Extrae caracteres chinos del archivo zh_hans.ini"""
